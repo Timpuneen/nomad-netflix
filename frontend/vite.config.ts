5f44a0cb99d242af6f0134ae4764fa0730fa61b1
timpuneen <timpuh_work@mail.ru>
2026-05-25 15:57:19 +0500

    fix: front port fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   envDir: "../", 
   server: {
+    port: 3000,
     proxy: {
       "/api": {
         target: process.env.VITE_API_URL || "http://localhost:8000",
